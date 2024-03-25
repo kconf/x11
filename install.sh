7f5f66787da9da27d15a9db3c108e54f76043cab
Jianwei Han <hanjianwei@gmail.com>
2024-03-25 17:37:09 +0800

    Add config

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed --noconfirm xdg-utils xorg-server xorg-xinit xorg-xrandr autorandr arandr i3 i3status-rust rofi wezterm maim xclip thunar
+paru -S --needed --noconfirm i3wsr
