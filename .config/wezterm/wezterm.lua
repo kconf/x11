ecf236c103566f7c45f74f2b9dd2290808a93895
Jianwei Han <hanjianwei@gmail.com>
2024-06-22 07:45:27 +0800

    wezterm: enable ime

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+config.use_ime = true
 
 -- For example, changing the color scheme:
 config.color_scheme = "AdventureTime"
