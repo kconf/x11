7fddefcf784d45d69d817ec1916d795bc8cf0bdb
Jianwei Han <hanjianwei@gmail.com>
2024-09-02 10:41:19 +0800

    feat(app): Add rofi-calc
    Changelog: feature

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed --noconfirm xdg-utils xorg-server xorg-xinit xorg-xrandr autorandr arandr i3 i3status-rust rofi wezterm maim xclip thunar
+sudo pacman -S --needed --noconfirm xdg-utils xorg-server xorg-xinit xorg-xrandr autorandr arandr i3 i3status-rust rofi rofi-calc wezterm maim xclip thunar
 paru -S --needed --noconfirm i3wsr
