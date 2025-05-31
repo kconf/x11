1a9b6fc9503e3abafa3f87f6cf705161e0eb9dc6
Jianwei Han <hanjianwei@gmail.com>
2025-05-31 18:45:01 +0800

    fix(wezterm): adjust scroll wheel behavior for Wayland

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,21 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.use_ime = true
 
+-- Fix scroll wheel behavior in wayland
+local act = wezterm.action
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByLine(-3),
+	},
+	{
+		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByLine(3),
+	},
+}
+
 -- For example, changing the color scheme:
 config.color_scheme = "GruvboxDark"
 
