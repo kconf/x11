3848219b55ea7f855ddc616e39b1615928e56ae8
Jianwei Han <hanjianwei@gmail.com>
2025-05-31 14:38:04 +0800

    feat(install): add polybar, rofi-wayland and zenity to dependencies

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed --noconfirm xdg-utils xorg-server xorg-xinit xorg-xrandr autorandr arandr i3 i3status-rust rofi rofi-calc wezterm maim xclip thunar
+sudo pacman -S --needed --noconfirm xdg-utils xorg-server xorg-xinit xorg-xrandr autorandr arandr i3 i3status-rust polybar rofi-wayland rofi-calc wezterm maim xclip thunar zenity
 paru -S --needed --noconfirm i3wsr
